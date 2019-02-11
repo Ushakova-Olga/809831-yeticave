9bb6455f05b30efe1820287a466cb88ff61401e8
Ushakova-Olga <Ushakova-Olga@yandex.ru>
2019-02-11 14:47:30 +0700

    Задание 4.2

--- schema.sql
+++ schema.sql
@@ -14,6 +14,14 @@ CREATE TABLE categories (
 );
 
 /* Лоты */
+/* В задании 4.2 требуется выводить текущую цену лота,которая рассчитывается
+как максимальная ставка либо если ставок нет - то берется начальная цена.
+Поскольку я не знаю как средствами SQL сделать по-другому, я делаю поле curr_price
+и в него пишу при каждом обновлении данных по лоту и привязанным к нему ставкам новую цену.
+Тогда в этом поле всегда находятся актуальные данные. Возможно не лучшее
+решение и потом придется убрать это поле и сделать по-другому.
+Поскольку в ТЗ в списке полей текущая цена не указана. */
+
 CREATE TABLE lots (
   id INT AUTO_INCREMENT PRIMARY KEY,
   date_add TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -21,6 +29,7 @@ CREATE TABLE lots (
   description TEXT,
   img_url CHAR(255),
   start_price INT NOT NULL,
+  curr_price INT,
   date_end TIMESTAMP,
   step INT NOT NULL,
   user_author_id INT NOT NULL,
@@ -48,37 +57,9 @@ CREATE TABLE users (
   contacts CHAR(255)
 );
 
-/* Для тренировки создала данные в таблицах похожие на те что были в массивах*/
-INSERT INTO categories(name) VALUES ('Доски и лыжи'), ('Крепления'), ('Ботинки'), ('Одежда'), ('Инструменты'), ('Разное');
-
-INSERT INTO lots SET  name='2014 Rossignol District Snowboard', img_url='img/lot-1.jpg', start_price=10999, step='100', user_author_id=3, category_id=1;
-INSERT INTO lots SET  name='DC Ply Mens 2016/2017 Snowboard', img_url='img/lot-2.jpg', start_price=159999, step='100', user_author_id=2, category_id=1;
-INSERT INTO lots SET  name='Крепления Union Contact Pro 2015 года размер L/XL', img_url='img/lot-3.jpg', start_price=8000, step='100', user_author_id=3, category_id=2;
-INSERT INTO lots SET  name='Ботинки для сноуборда DC Mutiny Charocal', img_url='img/lot-4.jpg', start_price=10999, step='100', user_author_id=1, category_id=3;
-INSERT INTO lots SET  name='Куртка для сноуборда DC Mutiny Charocal', img_url='img/lot-5.jpg', start_price=7500, step='100', user_author_id=1, category_id=4;
-INSERT INTO lots SET  name='Маска Oakley Canopy', img_url='img/lot-6.jpg', start_price=5400, step='100', user_author_id=1, category_id=6;
-
-/* Таблицу с пользователями заполнила произвольными значениями для тренировки */
-INSERT INTO users SET email='test1@ya.ru', name='Геннадий', password='123', contacts='2-222-222';
-INSERT INTO users SET email='test2@ya.ru', name='Николай', password='543', contacts='3-333-333';
-INSERT INTO users SET email='test3@ya.ru', name='Анна', password='096', contacts='4-444-444';
-
 /* Созданы индексы для поиска - уникальные и обычные*/
 CREATE UNIQUE INDEX category_name ON categories(name);
 CREATE UNIQUE INDEX user_email ON users(email);
 CREATE UNIQUE INDEX user_name ON users(name);
 CREATE INDEX lot_name ON lots(name);
 CREATE INDEX lot_start_price ON lots(start_price);
-
-/* Потренировалась выбирать по связанным значениям*/
-/* Лоты пользователя 3- Анна*/
-/*SELECT l.name, l.start_price FROM lots l
-JOIN users u
-ON l.user_author_id=u.id
-WHERE u.id=3;*/
-
-/* Лоты в категории 1 - Доски и лыжи*/
-/*SELECT l.name, l.start_price FROM lots l
-JOIN categories c
-ON l.category_id=c.id
-WHERE c.id=1;*/
